76626595f61c216540593a305d27edfbf04cb17f
Dennis Philippe Pereyra <DPPereyra>
2022-07-07 19:48:08 +0800

    Update podman and vagrant functions

--- station/runcom/functions/podman.zsh
+++ station/runcom/functions/podman.zsh
@@ -1,6 +1,6 @@
-# update-docker-images() {
-#   podman image prune
-#   podman images --filter "dangling=false" --format "{{.Repository}}:{{.Tag}}" \
-#     | xargs -L1 podman pull
-#   podman image prune
-# }
+update-podman-images() {
+  podman image prune
+  podman images --filter "dangling=false" --format "{{.Repository}}:{{.Tag}}" \
+    | xargs -L1 podman pull
+  podman image prune
+}
