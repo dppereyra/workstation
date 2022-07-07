01b95e286c963baec33e0eba15baaedcf8c6fbf3
Dennis Philippe Pereyra <DPPereyra>
2022-07-07 19:47:33 +0800

    Move mirror functions as it is distro dependent

--- station/runcom/system/distro/arch.zsh
+++ station/runcom/system/distro/arch.zsh
@@ -2,3 +2,16 @@ update-system() {
   sudo pacman -Syu
   yay -Syu --aur
 }
+
+update-mirrors() {
+  sudo reflector \
+    -a 24 \
+    -f 50 \
+    -l 200 \
+    --verbose \
+    --threads 10 \
+    -p https,http,ftp \
+    --download-timeout 60 \
+    --connection-timeout 30 \
+    --save /etc/pacman.d/mirrorlist
+}
