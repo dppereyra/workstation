5c9469eb03be0ace84f17d51b009d824c4385cfc
DPPereyra <dppereyra@gmail.com>
2022-08-17 23:42:44 +0800

    Disable some variables causing issues using kde terminals i.e. konsole and yakuake

--- station/runcom/s03_variables.zsh
+++ station/runcom/s03_variables.zsh
@@ -10,15 +10,15 @@ echo "Loading environment variables ..."
 
 export DOTNET_CLI_TELEMETRY_OPTOUT=1
 
-export EDITOR=vim
+# export EDITOR=vim
 
 export HISTFILE=~/station/zsh_history
 export HISTSIZE=1000000
 export SAVEHIST=1000000
 
-export TERM="xterm-256color"
+# export TERM="xterm-256color"
 
-export ZSH_DISABLE_COMPFIX=true
+# export ZSH_DISABLE_COMPFIX=true
 
 export VAGRANT_DEFAULT_PROVIDER=libvirt
 
