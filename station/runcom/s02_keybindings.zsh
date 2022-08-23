63fa8f82ad37b74d75fa35999c6ef57f7530269b
DPPereyra <dppereyra@gmail.com>
2022-08-23 14:53:00 +0800

    Add commented vi mode keybindings

--- station/runcom/s02_keybindings.zsh
+++ station/runcom/s02_keybindings.zsh
@@ -9,6 +9,7 @@
 echo "Configuring key bindings ..."
 
 bindkey -e
+# bindkey -v
 
 bindkey '^[[3~' delete-char
 
@@ -17,3 +18,31 @@ bindkey '^[[H'  beginning-of-line
 
 bindkey '^[[F'  end-of-line
 # bindkey '^[[8~' end-of-line
+
+# Use vim keys in tab complete menu:
+# bindkey -v '^?' backward-delete-char
+# bindkey -M menuselect 'h' vi-backward-char
+# bindkey -M menuselect 'k' vi-up-line-or-history
+# bindkey -M menuselect 'l' vi-forward-char
+# bindkey -M menuselect 'j' vi-down-line-or-history
+
+# Change cursor shape for different vi modes.
+# function zle-keymap-select {
+#   if [[ ${KEYMAP} == vicmd ]] ||
+#      [[ $1 = 'block' ]]; then
+#     echo -ne '\e[1 q'
+#   elif [[ ${KEYMAP} == main ]] ||
+#        [[ ${KEYMAP} == viins ]] ||
+#        [[ ${KEYMAP} = '' ]] ||
+#        [[ $1 = 'beam' ]]; then
+#     echo -ne '\e[5 q'
+#   fi
+# }
+# zle -N zle-keymap-select
+# zle-line-init() {
+#     zle -K viins # initiate `vi insert` as keymap (can be removed if `bindkey -V` has been set elsewhere)
+#     echo -ne "\e[5 q"
+# }
+# zle -N zle-line-init
+# echo -ne '\e[5 q' # Use beam shape cursor on startup.
+# preexec() { echo -ne '\e[5 q' ;} # Use beam shape cursor for each new prompt.
