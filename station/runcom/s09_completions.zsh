2ddd233209814634a34e0711f721c3acc8c5a41e
DPPereyra <mail@dppereyra.me>
2024-02-20 02:36:45 +0800

    Remove notifications in completions

--- station/runcom/s09_completions.zsh
+++ station/runcom/s09_completions.zsh
@@ -39,35 +39,25 @@ eval "$(rbenv init -)"
 if [ -x "`which npm`" ]; then
   echo "Loading npm completions ..."
   source <(npm completion)
-else
-  notify-send "No npm detected ..." || :
 fi
 
 if [ -x "`which doctl`" ]; then
   echo "Loading digitalocean completions ..."
   source <(doctl completion zsh)
-else
-  notify-send "No doctl detected ..." || :
 fi
 
 if [ -d $STATION_SDK/google-cloud-sdk ]; then
   echo "Loading Google SDK ..."
   source $STATION_SDK/google-cloud-sdk/path.zsh.inc
   source $STATION_SDK/google-cloud-sdk/completion.zsh.inc
-else
-  notify-send "No google sdk detected ..." || :
 fi
 
 if [ -d $FLUTTER_BIN ]; then
   echo "Loading flutter completions ..."
   source <(flutter zsh-completion)
-else
-  notify-send "No flutter sdk detected ..." || :
 fi
 
 if [ -x "`which zoxide`"]; then
   echo "Loading zoxide completions ..."
   eval "$(zoxide init zsh)"
-else
-  notify-send "No zoxide detected ..." || :
-fi
+ßfi
