6e1ddec548ef7c02800fb25d490cb1845d336a2a
DPPereyra <mail@dppereyra.me>
2024-04-09 17:49:11 +0800

    Update zsh completions

--- station/runcom/s09_completions.zsh
+++ station/runcom/s09_completions.zsh
@@ -15,11 +15,10 @@ fpath=(${ASDF_DIR}/completions $fpath)
 echo "Loading pipx completions ..."
 eval "$(register-python-argcomplete pipx)"
 
+echo "Checking for kubectl ..."
 if [[ -x "`which kubectl`" ]]; then
   echo "Loading kubectl completions ..."
   source <(kubectl completion zsh)
-else
-  notify-send "No kubectl detected ..." || :
 fi
 
 echo "Loading pyenv ..."
@@ -36,28 +35,33 @@ eval "$(nodenv init -)"
 echo "Loading rbenv ..."
 eval "$(rbenv init -)"
 
-if [ -x "`which npm`" ]; then
+echo "Checking for npm ..."
+if [[ -x "`which npm`" ]]; then
   echo "Loading npm completions ..."
   source <(npm completion)
 fi
 
-if [ -x "`which doctl`" ]; then
+echo "Checking for digitalocean cli tool ..."
+if [[ -x "`which doctl`" ]]; then
   echo "Loading digitalocean completions ..."
   source <(doctl completion zsh)
 fi
 
-if [ -d $STATION_SDK/google-cloud-sdk ]; then
+echo "Checking for google sdk ..."
+if [[ -d $STATION_SDK/google-cloud-sdk ]]; then
   echo "Loading Google SDK ..."
   source $STATION_SDK/google-cloud-sdk/path.zsh.inc
   source $STATION_SDK/google-cloud-sdk/completion.zsh.inc
 fi
 
-if [ -d $FLUTTER_BIN ]; then
+echo "Checking for flutter ..."
+if [[ -d $FLUTTER_BIN ]]; then
   echo "Loading flutter completions ..."
   source <(flutter zsh-completion)
 fi
 
-if [ -x "`which zoxide`"]; then
+echo "Checking for zoxide ..."
+if [[ -x $(which zoxide) ]]; then
   echo "Loading zoxide completions ..."
-  eval "$(zoxide init zsh)"
-ßfi
+  source <(zoxide init zsh)
+fi
