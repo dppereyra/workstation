065df787f18797d9409ae546bf586fc679da66b7
DPPereyra <mail@dppereyra.me>
2024-07-02 00:52:59 +0800

    Additional zsh environment variables

--- station/runcom/s03_variables.zsh
+++ station/runcom/s03_variables.zsh
@@ -11,18 +11,17 @@ echo "Loading environment variables ..."
 export DOTNET_CLI_TELEMETRY_OPTOUT=1
 
 # export EDITOR=vim
-
-export HISTFILE=~/station/zsh_history
-export HISTSIZE=1000000
-export SAVEHIST=1000000
-
 # export TERM="xterm-256color"
-
 # export ZSH_DISABLE_COMPFIX=true
 
-export VAGRANT_DEFAULT_PROVIDER=libvirt
+export HISTFILE=~/station/zsh_history
+export HISTSIZE=200000
+export SAVEHIST=100000
 
-export ZINIT_HOME=~/station/zinit
+export VAGRANT_DEFAULT_PROVIDER=libvirt
+export PYTHONWARNINGS="ignore:Unverified HTTPS request"
+
+export ZINIT_HOME="~/station/zinit"
 
 if [[ ! -f $STATION_RC/s98_secrets.zsh ]]
 then
