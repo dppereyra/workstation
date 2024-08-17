8b20ac31a3c2389928282845e9ec91df769b1178
DPPereyra <mail@dppereyra.me>
2024-08-17 23:40:33 +0800

    Update zsh keybindings config

--- station/runcom/s02_keybindings.zsh
+++ station/runcom/s02_keybindings.zsh
@@ -13,10 +13,10 @@ bindkey -e
 
 bindkey '^[[3~' delete-char
 
-bindkey '^[[H'  beginning-of-line
+# bindkey '^[[H'  beginning-of-line
 # bindkey '^[[7~' beginning-of-line
 
-bindkey '^[[F'  end-of-line
+# bindkey '^[[F'  end-of-line
 # bindkey '^[[8~' end-of-line
 
 # Use vim keys in tab complete menu:
