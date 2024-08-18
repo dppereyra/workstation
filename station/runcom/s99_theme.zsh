3515d2e4e4dbd589aff48e7833ca319bce97fd36
DPPereyra <mail@dppereyra.me>
2024-08-18 21:06:26 +0800

    Switch back to starship theme

--- station/runcom/s99_theme.zsh
+++ station/runcom/s99_theme.zsh
@@ -9,13 +9,15 @@
 echo "Configuring theme ..."
 
 
-zinit ice depth=1; zinit light romkatv/powerlevel10k
-[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+# zinit ice depth=1; zinit light romkatv/powerlevel10k
+# [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
 
 # function blastoff(){
 #   echo "🚀"
 # }
 # starship_precmd_user_func="blastoff"
-# eval "$(starship init zsh)"
+
+eval "$(starship init zsh)"
 
 # eval "$(oh-my-posh --init --shell zsh --config $STATION_HOME/powerlevel10k_classic.omp.json)"
+
